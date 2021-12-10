de8aa7e1485afd1c4859e84edc42d5e63646df85
Danny <danielt1melb@gmail.com>
2021-12-10 14:35:25 +1100

    Scheduler

---
+++ Runtime/Schedule System/Schedule.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Schedule : MonoBehaviour
+{
+    public static Schedule Instance;
+    public List<ScheduledTask> tasks = new List<ScheduledTask>();
+    void Start()
+    {
+        CreateInstance();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CreateInstance();
+
+        for (int i = tasks.Count - 1; i >= 0; i--)
+        {
+            ScheduledTask task = tasks[i];
+            if (task.Tick())
+            {
+                tasks.RemoveAt(i);
+            }
+        }
+
+    }
+
+    public static ScheduledTask Add(float wait, UnityAction<ScheduledTask> callback, float repeat = 0)
+    {
+        CreateInstance();
+        ScheduledTask task = new ScheduledTask(wait, callback, repeat);
+        Instance.tasks.Add(task);
+        return task;
+    }
+    public static ScheduledTask Add(ScheduledTask task)
+    {
+        CreateInstance();
+        Instance.tasks.Add(task);
+        return task;
+    }
+
+
+    public static void CreateInstance()
+    {
+        if (Instance != null) { return; }
+        GameObject obj = GameObject.FindGameObjectWithTag("Util");
+        if (obj != null)
+        {
+            Schedule comp = obj.GetComponent<Schedule>();
+            if (comp != null)
+            {
+                Instance = comp;
+                return;
+            }
+        }
+        GameObject newObj = new GameObject("Scheduler");
+        newObj.tag = "Util";
+        Instance = newObj.AddComponent<Schedule>();
+    }
+}
+
+
+
+
+
+
+
---
+++ Runtime/Schedule System/ScheduledTask.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class ScheduledTask
+{
+    float delay;
+    UnityAction<ScheduledTask> callback;
+    float passedTime = 0;
+    float delayBetweenRepeat = 0;
+    int numberOfTimesToRepeat = 0;
+    int callCount = 0;
+    bool shouldRemove = false;
+    public ScheduledTask(float _delay, UnityAction<ScheduledTask> _callback, float _repeatDelay = 0)
+    {
+        delay = _delay;
+        callback = _callback;
+        delayBetweenRepeat = _repeatDelay;
+        if (delayBetweenRepeat != 0)
+        {
+            numberOfTimesToRepeat = -1;
+        }
+
+    }
+    public virtual bool Tick()
+    {
+        if (shouldRemove) { return true; }
+        passedTime += Time.deltaTime;
+        if (passedTime >= delay)
+        {
+            callback.Invoke(this);
+            callCount++;
+            if (numberOfTimesToRepeat > 0 || numberOfTimesToRepeat == -1)// If it should repeat
+            {
+                if (numberOfTimesToRepeat > 0) { numberOfTimesToRepeat--; }
+                delay = delayBetweenRepeat;
+                passedTime -= delay;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public ScheduledTask Wait(float seconds)
+    {
+        delay = seconds;
+        return this;
+    }
+    public ScheduledTask Repeat(float _delay, int _count = -1)
+    {
+        delayBetweenRepeat = _delay;
+        numberOfTimesToRepeat = _count;
+        return this;
+    }
+    public ScheduledTask SetCallback(UnityAction<ScheduledTask> _callback)
+    {
+        callback = _callback;
+        return this;
+    }
+    public ScheduledTask Cancel()
+    {
+        shouldRemove = true;
+        return this;
+    }
+    public int CallCount { get { return callCount; } }
+
+
+
+
+}
