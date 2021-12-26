3ffb9585f0181e5047709d94574bf275730d804f
Danny <danielt1melb@gmail.com>
2021-12-27 00:21:37 +1100

    Quick Replace UI

--- Runtime/Editor/QuickReplaceEditor.cs
+++ Runtime/Editor/QuickReplaceEditor.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using Dutil;
 public class QuickReplaceEditor : EditorWindow
 {
-    public GameObject newObject;
+    public GameObject newPrefab;
     public List<GameObject> toReplace = new List<GameObject>();
     public List<GameObject> converted = new List<GameObject>();
 
@@ -15,34 +15,41 @@ public class QuickReplaceEditor : EditorWindow
     static void Init()
     {
         // Get existing open window or if none, make a new one:
-        QuickReplaceEditor window = (QuickReplaceEditor)EditorWindow.GetWindow(typeof(QuickReplaceEditor));
+        QuickReplaceEditor window = (QuickReplaceEditor)EditorWindow.GetWindow(typeof(QuickReplaceEditor), false, "Quick Replace");
         window.Show();
     }
     void OnGUI()
     {
         Color oldBG = GUI.backgroundColor;
         EditorGUILayout.BeginHorizontal();
-
+        GUI.backgroundColor = Colours.Orange.Shade(2);
         if (Selection.gameObjects.Length > 0)
         {
-            if (GUILayout.Button("Pull Selected"))
+            if (GUILayout.Button("Pull Selected", new GUILayoutOption[] { GUILayout.Height(24) }))
             {
                 toReplace.Clear();
                 toReplace = Selection.gameObjects.ToList();
             }
         }
-        if (GUILayout.Button("Reset"))
+
+
+        if (GUILayout.Button("Clear", new GUILayoutOption[] { GUILayout.Height(24) }))
+        {
+            toReplace.Clear();
+        }
+        GUI.backgroundColor = Colours.Red.Shade(2);
+        if (GUILayout.Button("Reset", new GUILayoutOption[] { GUILayout.Height(24) }))
         {
             toReplace.Clear();
             converted.Clear();
-            newObject = null;
+            newPrefab = null;
         }
-
+        EditorGUILayout.EndHorizontal();
         bool performed = false;
-        GUI.backgroundColor = Colours.Green;
-        if (GUILayout.Button("Replace"))
+        GUI.backgroundColor = Colours.Blue.Shade(2);
+        if (GUILayout.Button("Replace", new GUILayoutOption[] { GUILayout.Height(32) }))
         {
-            if (newObject == null)
+            if (newPrefab == null)
             {
                 Debug.LogError("Need to set a new prefab to replace with");
             }
@@ -50,34 +57,52 @@ public class QuickReplaceEditor : EditorWindow
             {
                 toReplace.Clean();
                 converted.Clear();
-
+                Undo.SetCurrentGroupName("replaceables");
                 for (int i = 0; i < toReplace.Count; i++)
                 {
                     GameObject old = toReplace[i];
-                    GameObject spawned = GameObject.Instantiate(newObject, old.transform.position, old.transform.rotation);
+                    GameObject spawned = GameObject.Instantiate(newPrefab, old.transform.position, old.transform.rotation);
                     spawned.transform.localScale = old.transform.localScale;
                     spawned.name = old.name;
                     converted.Add(spawned);
                     Undo.RegisterCreatedObjectUndo(spawned, "Replaced using Dutil");
                 }
-                Undo.RecordObjects(toReplace.ToArray(), "Removed by Dutil");
-                toReplace.ForEach(x => DestroyImmediate(x));
+
+                toReplace.ForEach(x => Undo.DestroyObjectImmediate(x));
                 toReplace.Clear();
                 performed = true;
             }
         }
-        GUI.backgroundColor = oldBG;
-        EditorGUILayout.EndHorizontal();
+        GUILayout.Label("");
         ScriptableObject scriptableObj = this;
         SerializedObject serialObj = new SerializedObject(scriptableObj);
-        SerializedProperty list = serialObj.FindProperty("toReplace");
-        SerializedProperty convertedList = serialObj.FindProperty("converted");
-        SerializedProperty newItem = serialObj.FindProperty("newObject");
 
+        SerializedProperty list = serialObj.FindProperty("toReplace");
+
+        SerializedProperty convertedList = serialObj.FindProperty("converted");
+
+        SerializedProperty newItem = serialObj.FindProperty("newPrefab");
+
+        GUI.backgroundColor = Colours.Blue.Shade(1);
         EditorGUILayout.PropertyField(newItem, true);
+
+        GUI.backgroundColor = Colours.Orange.Shade(2);
         EditorGUILayout.PropertyField(list, !performed);
+
+        GUI.backgroundColor = oldBG;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("^^^", new GUILayoutOption[] { GUILayout.Height(24), GUILayout.Width(60) }))
+        {
+            toReplace = converted.Copy();
+            converted.Clear();
+        }
+        GUILayout.EndHorizontal();
+        GUI.backgroundColor = Colours.Blue.Shade(2);
         EditorGUILayout.PropertyField(convertedList, true);
         serialObj.ApplyModifiedProperties();
+        GUI.backgroundColor = oldBG;
 
 
 
