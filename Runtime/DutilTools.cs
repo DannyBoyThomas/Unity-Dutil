dc60e7297c6e816b0668ef332b9e87002fb56b4c
DannyBoyThomas <danielt1melb@gmail.com>
2022-08-23 15:17:50 +1000

    Update Checker

--- Runtime/DutilTools.cs
+++ Runtime/DutilTools.cs
@@ -94,7 +94,7 @@ public class DutilTools
             Debug.Log("Dutil is already checking for update.");
             return;
         }
-        searchRequest = UnityEditor.PackageManager.Client.Search("com.dannyboythomas.dutil");
+        searchRequest = UnityEditor.PackageManager.Client.Search("dannyboythomas.dutil");
         EditorApplication.update += SearchProgress;
 
     }
