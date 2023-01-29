d8c01d160be72b2f5bfd67e761cfe18faeda0c3b
DannyBoyThomas <danielt1melb@gmail.com>
2023-01-29 18:10:11 +1100

    UI fix - Works in bulit projects

--- Runtime/Editor/DUI_Height_Editor.cs
+++ Runtime/Editor/DUI_Height_Editor.cs
@@ -2,130 +2,136 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Dutil;
+#if UNITY_EDITOR
 using UnityEditor;
 using System;
 using System.Reflection;
 
-[CustomEditor(typeof(DUI_Height))]
-public class DUI_Height_Editor : Editor
+namespace Dutil
 {
-    DUI_Height Target;
-    bool showMinHeight = false;
-    bool showMaxHeight = false;
-    bool showPadding = false;
-    private void OnEnable()
+
+    [CustomEditor(typeof(DUI_Height))]
+    public class DUI_Height_Editor : Editor
     {
-        Target = (DUI_Height)base.target;
+        DUI_Height Target;
+        bool showMinHeight = false;
+        bool showMaxHeight = false;
+        bool showPadding = false;
+        private void OnEnable()
+        {
+            Target = (DUI_Height)base.target;
+        }
+        public override void OnInspectorGUI()
+        {
+            int spacing = 6;
+
+
+            //get serialized property
+            SerializedProperty forceToCenter = serializedObject.FindProperty("forceToCenter");
+            SerializedProperty targetHeight = serializedObject.FindProperty("targetHeight");
+            SerializedProperty useTargetPercentage = serializedObject.FindProperty("useTargetPercentage");
+            SerializedProperty useMinHeight = serializedObject.FindProperty("useMinHeight");
+            SerializedProperty useMaxHeight = serializedObject.FindProperty("useMaxHeight");
+            SerializedProperty usePadding = serializedObject.FindProperty("usePadding");
+
+
+            EditorGUILayout.PropertyField(forceToCenter);
+            EditorGUILayout.PropertyField(targetHeight);
+            //target percentage
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PropertyField(useTargetPercentage);
+            if (useTargetPercentage.boolValue)
+            {
+                GUILayout.Space(spacing);
+                Target.targetHeightPercentage = EditorGUILayout.Slider(Target.targetHeightPercentage, 0, 100);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            //min height  
+
+            showMinHeight = EditorGUILayout.BeginFoldoutHeaderGroup(showMinHeight, "Minimum Height");
+            if (showMinHeight)
+            {
+
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Space(spacing * 3);
+                EditorGUILayout.PropertyField(useMinHeight);
+                EditorGUILayout.EndHorizontal();
+
+                if (useMinHeight.boolValue)
+                {
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty minHeight = serializedObject.FindProperty("minHeight");
+                    EditorGUILayout.PropertyField(minHeight);
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty maxHeightUnit = serializedObject.FindProperty("maxHeightUnit");
+                    EditorGUILayout.PropertyField(maxHeightUnit);
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            //max height
+            showMaxHeight = EditorGUILayout.BeginFoldoutHeaderGroup(showMaxHeight, "Maximum Height");
+            if (showMaxHeight)
+            {
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Space(spacing * 3);
+                EditorGUILayout.PropertyField(useMaxHeight);
+                EditorGUILayout.EndHorizontal();
+
+                if (useMaxHeight.boolValue)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty maxHeight = serializedObject.FindProperty("maxHeight");
+                    EditorGUILayout.PropertyField(maxHeight);
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty maxHeightUnit = serializedObject.FindProperty("maxHeightUnit");
+                    EditorGUILayout.PropertyField(maxHeightUnit);
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+
+            //padding
+            showPadding = EditorGUILayout.BeginFoldoutHeaderGroup(showPadding, "Padding");
+            if (showPadding)
+            {
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Space(spacing * 3);
+                EditorGUILayout.PropertyField(usePadding);
+                EditorGUILayout.EndHorizontal();
+
+                if (usePadding.boolValue)
+                {
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty padding = serializedObject.FindProperty("padding");
+                    EditorGUILayout.PropertyField(padding);
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty paddingUnit = serializedObject.FindProperty("paddingUnit");
+                    EditorGUILayout.PropertyField(paddingUnit);
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            serializedObject.ApplyModifiedProperties();
+
+
+        }
+
     }
-    public override void OnInspectorGUI()
-    {
-        int spacing = 6;
-
-
-        //get serialized property
-        SerializedProperty forceToCenter = serializedObject.FindProperty("forceToCenter");
-        SerializedProperty targetHeight = serializedObject.FindProperty("targetHeight");
-        SerializedProperty useTargetPercentage = serializedObject.FindProperty("useTargetPercentage");
-        SerializedProperty useMinHeight = serializedObject.FindProperty("useMinHeight");
-        SerializedProperty useMaxHeight = serializedObject.FindProperty("useMaxHeight");
-        SerializedProperty usePadding = serializedObject.FindProperty("usePadding");
-
-
-        EditorGUILayout.PropertyField(forceToCenter);
-        EditorGUILayout.PropertyField(targetHeight);
-        //target percentage
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.PropertyField(useTargetPercentage);
-        if (useTargetPercentage.boolValue)
-        {
-            GUILayout.Space(spacing);
-            Target.targetHeightPercentage = EditorGUILayout.Slider(Target.targetHeightPercentage, 0, 100);
-        }
-        EditorGUILayout.EndHorizontal();
-
-        //min height  
-
-        showMinHeight = EditorGUILayout.BeginFoldoutHeaderGroup(showMinHeight, "Minimum Height");
-        if (showMinHeight)
-        {
-
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Space(spacing * 3);
-            EditorGUILayout.PropertyField(useMinHeight);
-            EditorGUILayout.EndHorizontal();
-
-            if (useMinHeight.boolValue)
-            {
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty minHeight = serializedObject.FindProperty("minHeight");
-                EditorGUILayout.PropertyField(minHeight);
-                EditorGUILayout.EndHorizontal();
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty maxHeightUnit = serializedObject.FindProperty("maxHeightUnit");
-                EditorGUILayout.PropertyField(maxHeightUnit);
-                EditorGUILayout.EndHorizontal();
-            }
-        }
-        EditorGUILayout.EndFoldoutHeaderGroup();
-        //max height
-        showMaxHeight = EditorGUILayout.BeginFoldoutHeaderGroup(showMaxHeight, "Maximum Height");
-        if (showMaxHeight)
-        {
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Space(spacing * 3);
-            EditorGUILayout.PropertyField(useMaxHeight);
-            EditorGUILayout.EndHorizontal();
-
-            if (useMaxHeight.boolValue)
-            {
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty maxHeight = serializedObject.FindProperty("maxHeight");
-                EditorGUILayout.PropertyField(maxHeight);
-                EditorGUILayout.EndHorizontal();
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty maxHeightUnit = serializedObject.FindProperty("maxHeightUnit");
-                EditorGUILayout.PropertyField(maxHeightUnit);
-                EditorGUILayout.EndHorizontal();
-            }
-        }
-        EditorGUILayout.EndFoldoutHeaderGroup();
-
-        //padding
-        showPadding = EditorGUILayout.BeginFoldoutHeaderGroup(showPadding, "Padding");
-        if (showPadding)
-        {
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Space(spacing * 3);
-            EditorGUILayout.PropertyField(usePadding);
-            EditorGUILayout.EndHorizontal();
-
-            if (usePadding.boolValue)
-            {
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty padding = serializedObject.FindProperty("padding");
-                EditorGUILayout.PropertyField(padding);
-                EditorGUILayout.EndHorizontal();
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty paddingUnit = serializedObject.FindProperty("paddingUnit");
-                EditorGUILayout.PropertyField(paddingUnit);
-                EditorGUILayout.EndHorizontal();
-            }
-        }
-        EditorGUILayout.EndFoldoutHeaderGroup();
-        serializedObject.ApplyModifiedProperties();
-
-
-    }
-
 }
+#endif
--- Runtime/Editor/DUI_Width_Editor.cs
+++ Runtime/Editor/DUI_Width_Editor.cs
@@ -1,131 +1,136 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Dutil;
+
+#if UNITY_EDITOR
 using UnityEditor;
 using System;
 using System.Reflection;
 
-[CustomEditor(typeof(DUI_Width))]
-public class DUI_Width_Editor : Editor
+namespace Dutil
 {
-    DUI_Width Target;
-    bool showMinWidth = false;
-    bool showMaxWidth = false;
-    bool showPadding = false;
-    private void OnEnable()
+    [CustomEditor(typeof(DUI_Width))]
+    public class DUI_Width_Editor : Editor
     {
-        Target = (DUI_Width)base.target;
+        DUI_Width Target;
+        bool showMinWidth = false;
+        bool showMaxWidth = false;
+        bool showPadding = false;
+        private void OnEnable()
+        {
+            Target = (DUI_Width)base.target;
+        }
+        public override void OnInspectorGUI()
+        {
+            int spacing = 6;
+
+
+            //get serialized property
+            SerializedProperty forceToCenter = serializedObject.FindProperty("forceToCenter");
+            SerializedProperty targetWidth = serializedObject.FindProperty("targetWidth");
+            SerializedProperty useTargetPercentage = serializedObject.FindProperty("useTargetPercentage");
+            SerializedProperty useMinWidth = serializedObject.FindProperty("useMinWidth");
+            SerializedProperty useMaxWidth = serializedObject.FindProperty("useMaxWidth");
+            SerializedProperty usePadding = serializedObject.FindProperty("usePadding");
+
+
+            EditorGUILayout.PropertyField(forceToCenter);
+            EditorGUILayout.PropertyField(targetWidth);
+            //target percentage
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PropertyField(useTargetPercentage);
+            if (useTargetPercentage.boolValue)
+            {
+                GUILayout.Space(spacing);
+                Target.targetWidthPercentage = EditorGUILayout.Slider(Target.targetWidthPercentage, 0, 100);
+            }
+            EditorGUILayout.EndHorizontal();
+
+            //min width  
+
+            showMinWidth = EditorGUILayout.BeginFoldoutHeaderGroup(showMinWidth, "Minimum Width");
+            if (showMinWidth)
+            {
+
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Space(spacing * 3);
+                EditorGUILayout.PropertyField(useMinWidth);
+                EditorGUILayout.EndHorizontal();
+
+                if (useMinWidth.boolValue)
+                {
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty minWidth = serializedObject.FindProperty("minWidth");
+                    EditorGUILayout.PropertyField(minWidth);
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty maxWidthUnit = serializedObject.FindProperty("maxWidthUnit");
+                    EditorGUILayout.PropertyField(maxWidthUnit);
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            //max width
+            showMaxWidth = EditorGUILayout.BeginFoldoutHeaderGroup(showMaxWidth, "Maximum Width");
+            if (showMaxWidth)
+            {
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Space(spacing * 3);
+                EditorGUILayout.PropertyField(useMaxWidth);
+                EditorGUILayout.EndHorizontal();
+
+                if (useMaxWidth.boolValue)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty maxWidth = serializedObject.FindProperty("maxWidth");
+                    EditorGUILayout.PropertyField(maxWidth);
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty maxWidthUnit = serializedObject.FindProperty("maxWidthUnit");
+                    EditorGUILayout.PropertyField(maxWidthUnit);
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+
+            //padding
+            showPadding = EditorGUILayout.BeginFoldoutHeaderGroup(showPadding, "Padding");
+            if (showPadding)
+            {
+                EditorGUILayout.BeginHorizontal();
+                GUILayout.Space(spacing * 3);
+                EditorGUILayout.PropertyField(usePadding);
+                EditorGUILayout.EndHorizontal();
+
+                if (usePadding.boolValue)
+                {
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty padding = serializedObject.FindProperty("padding");
+                    EditorGUILayout.PropertyField(padding);
+                    EditorGUILayout.EndHorizontal();
+
+                    EditorGUILayout.BeginHorizontal();
+                    GUILayout.Space(spacing * 3);
+                    SerializedProperty paddingUnit = serializedObject.FindProperty("paddingUnit");
+                    EditorGUILayout.PropertyField(paddingUnit);
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            serializedObject.ApplyModifiedProperties();
+
+
+        }
+
     }
-    public override void OnInspectorGUI()
-    {
-        int spacing = 6;
-
-
-        //get serialized property
-        SerializedProperty forceToCenter = serializedObject.FindProperty("forceToCenter");
-        SerializedProperty targetWidth = serializedObject.FindProperty("targetWidth");
-        SerializedProperty useTargetPercentage = serializedObject.FindProperty("useTargetPercentage");
-        SerializedProperty useMinWidth = serializedObject.FindProperty("useMinWidth");
-        SerializedProperty useMaxWidth = serializedObject.FindProperty("useMaxWidth");
-        SerializedProperty usePadding = serializedObject.FindProperty("usePadding");
-
-
-        EditorGUILayout.PropertyField(forceToCenter);
-        EditorGUILayout.PropertyField(targetWidth);
-        //target percentage
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.PropertyField(useTargetPercentage);
-        if (useTargetPercentage.boolValue)
-        {
-            GUILayout.Space(spacing);
-            Target.targetWidthPercentage = EditorGUILayout.Slider(Target.targetWidthPercentage, 0, 100);
-        }
-        EditorGUILayout.EndHorizontal();
-
-        //min width  
-
-        showMinWidth = EditorGUILayout.BeginFoldoutHeaderGroup(showMinWidth, "Minimum Width");
-        if (showMinWidth)
-        {
-
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Space(spacing * 3);
-            EditorGUILayout.PropertyField(useMinWidth);
-            EditorGUILayout.EndHorizontal();
-
-            if (useMinWidth.boolValue)
-            {
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty minWidth = serializedObject.FindProperty("minWidth");
-                EditorGUILayout.PropertyField(minWidth);
-                EditorGUILayout.EndHorizontal();
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty maxWidthUnit = serializedObject.FindProperty("maxWidthUnit");
-                EditorGUILayout.PropertyField(maxWidthUnit);
-                EditorGUILayout.EndHorizontal();
-            }
-        }
-        EditorGUILayout.EndFoldoutHeaderGroup();
-        //max width
-        showMaxWidth = EditorGUILayout.BeginFoldoutHeaderGroup(showMaxWidth, "Maximum Width");
-        if (showMaxWidth)
-        {
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Space(spacing * 3);
-            EditorGUILayout.PropertyField(useMaxWidth);
-            EditorGUILayout.EndHorizontal();
-
-            if (useMaxWidth.boolValue)
-            {
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty maxWidth = serializedObject.FindProperty("maxWidth");
-                EditorGUILayout.PropertyField(maxWidth);
-                EditorGUILayout.EndHorizontal();
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty maxWidthUnit = serializedObject.FindProperty("maxWidthUnit");
-                EditorGUILayout.PropertyField(maxWidthUnit);
-                EditorGUILayout.EndHorizontal();
-            }
-        }
-        EditorGUILayout.EndFoldoutHeaderGroup();
-
-        //padding
-        showPadding = EditorGUILayout.BeginFoldoutHeaderGroup(showPadding, "Padding");
-        if (showPadding)
-        {
-            EditorGUILayout.BeginHorizontal();
-            GUILayout.Space(spacing * 3);
-            EditorGUILayout.PropertyField(usePadding);
-            EditorGUILayout.EndHorizontal();
-
-            if (usePadding.boolValue)
-            {
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty padding = serializedObject.FindProperty("padding");
-                EditorGUILayout.PropertyField(padding);
-                EditorGUILayout.EndHorizontal();
-
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Space(spacing * 3);
-                SerializedProperty paddingUnit = serializedObject.FindProperty("paddingUnit");
-                EditorGUILayout.PropertyField(paddingUnit);
-                EditorGUILayout.EndHorizontal();
-            }
-        }
-        EditorGUILayout.EndFoldoutHeaderGroup();
-        serializedObject.ApplyModifiedProperties();
-
-
-    }
-
 }
+#endif
