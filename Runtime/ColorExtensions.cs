0c8e6dc3590a2094bb5c36473c7c2be99f42496d
DannyBoyThomas <danielt1melb@gmail.com>
2023-07-09 15:07:53 +1000

    Update ColorExtensions.cs

--- Runtime/ColorExtensions.cs
+++ Runtime/ColorExtensions.cs
@@ -72,66 +72,70 @@ namespace Dutil
         public static List<Color> Shades(this Color col, bool includeBase = false)
         {
             List<Color> shades = new List<Color>();
-            if (Colours.All.Contains(col))
+
+            if (col == Colours.Red)
             {
-                if (col == Colours.Red)
-                {
-                    return shades = new List<Color>() { Hex("#ffebee"), Hex("ffcdd2"), Hex("ef9a9a"), Hex("e57373"), Hex("ef5350"), Hex("e53935"), Hex("d32f2f"), Hex("c62828"), Hex("b71c1c") };
-                }
-                if (col == Colours.Pink)
-                {
-                    return shades = new List<Color>() { Hex("fce4ec"), Hex("f8bbd0"), Hex("f48fb1"), Hex("f06292"), Hex("ec407a"), Hex("d81b60"), Hex("c2185b"), Hex("ad1457"), Hex("880e4f") };
-                }
-                if (col == Colours.Purple)
-                {
-                    return shades = new List<Color>() { Hex("f3e5f5"), Hex("e1bee7"), Hex("ce93d8"), Hex("ba68c8"), Hex("ab47bc"), Hex("8e24aa"), Hex("7b1fa2"), Hex("6a1b9a"), Hex("4a148c") };
-                }
-                if (col == Colours.DeepPurple)
-                {
-                    return shades = new List<Color>() { Hex("ede7f6"), Hex("d1c4e9"), Hex("b39ddb"), Hex("9575cd"), Hex("7e57c2"), Hex("5e35b1"), Hex("512da8"), Hex("4527a0"), Hex("311b92") };
-                }
-                if (col == Colours.Indigo)
-                {
-                    return shades = new List<Color>() { Hex("e8eaf6"), Hex("c5cae9"), Hex("9fa8da"), Hex("7986cb"), Hex("5c6bc0"), Hex("3949ab"), Hex("303f9f"), Hex("283593"), Hex("1a237e") };
-                }
-                if (col == Colours.Blue)
-                {
-                    return shades = new List<Color>() { Hex("e3f2fd"), Hex("bbdefb"), Hex("90caf9"), Hex("64b5f6"), Hex("42a5f5"), Hex("1e88e5"), Hex("1976d2"), Hex("1565c0"), Hex("0d47a1") };
-                }
-                if (col == Colours.Cyan)
-                {
-                    return shades = new List<Color>() { Hex("e0f7fa"), Hex("b2ebf2"), Hex("80deea"), Hex("4dd0e1"), Hex("26c6da"), Hex("00acc1"), Hex("0097a7"), Hex("00838f"), Hex("006064") };
-                }
-                if (col == Colours.Teal)
-                {
-                    return shades = new List<Color>() { Hex("e0f2f1"), Hex("b2dfdb"), Hex("80cbc4"), Hex("4db6ac"), Hex("26a69a"), Hex("00897b"), Hex("00796b"), Hex("00695c"), Hex("#004d40") };
-                }
-                if (col == Colours.Green)
-                {
-                    shades = new List<Color>() { Hex("e8f5e9"), Hex("c8e6c9"), Hex("a5d6a7"), Hex("81c784"), Hex("66bb6a"), Hex("43a047"), Hex("388e3c"), Hex("2e7d32"), Hex("1b5e20") };
-                }
-                if (col == Colours.Yellow)
-                {
-                    shades = new List<Color>() { Hex("fffde7"), Hex("fff9c4"), Hex("fff59d"), Hex("fff176"), Hex("ffee58"), Hex("fdd835"), Hex("fbc02d"), Hex("f9a825"), Hex("f57f17") };
-                }
-                if (col == Colours.Orange)
-                {
-                    shades = new List<Color>() { Hex("fff3e0"), Hex("ffe0b2"), Hex("ffcc80"), Hex("ffb74d"), Hex("ffa726"), Hex("fb8c00"), Hex("f57c00"), Hex("ef6c00"), Hex("e65100") };
-                }
-                if (col == Colours.Grey)
-                {
-                    shades = new List<Color>() { Hex("fafafa"), Hex("f5f5f5"), Hex("eeeeee"), Hex("e0e0e0"), Hex("9e9e9e"), Hex("757575"), Hex("616161"), Hex("424242"), Hex("212121") };
-                }
-                if (col == Colours.BlueGrey)
-                {
-                    shades = new List<Color>() { Hex("eceff1"), Hex("cfd8dc"), Hex("b0bec5"), Hex("90a4ae"), Hex("78909c"), Hex("546e7a"), Hex("455a64"), Hex("37474f"), Hex("263238") };
-                }
-                if (col == Colours.Brown)
-                {
-                    shades = new List<Color>() { Hex("efebe9"), Hex("d7ccc8"), Hex("bcaaa4"), Hex("a1887f"), Hex("8d6e63"), Hex("6d4c41"), Hex("5d4037"), Hex("4e342e"), Hex("3e2723") };
-                }
+                shades = new List<Color>() { Hex("#ffebee"), Hex("ffcdd2"), Hex("ef9a9a"), Hex("e57373"), Hex("ef5350"), Hex("e53935"), Hex("d32f2f"), Hex("c62828"), Hex("b71c1c") };
+            }
+            else if (col == Colours.Pink)
+            {
+                shades = new List<Color>() { Hex("fce4ec"), Hex("f8bbd0"), Hex("f48fb1"), Hex("f06292"), Hex("ec407a"), Hex("d81b60"), Hex("c2185b"), Hex("ad1457"), Hex("880e4f") };
+            }
+            else if (col == Colours.Purple)
+            {
+                shades = new List<Color>() { Hex("f3e5f5"), Hex("e1bee7"), Hex("ce93d8"), Hex("ba68c8"), Hex("ab47bc"), Hex("8e24aa"), Hex("7b1fa2"), Hex("6a1b9a"), Hex("4a148c") };
+            }
+            else if (col == Colours.DeepPurple)
+            {
+                shades = new List<Color>() { Hex("ede7f6"), Hex("d1c4e9"), Hex("b39ddb"), Hex("9575cd"), Hex("7e57c2"), Hex("5e35b1"), Hex("512da8"), Hex("4527a0"), Hex("311b92") };
+            }
+            else if (col == Colours.Indigo)
+            {
+                shades = new List<Color>() { Hex("e8eaf6"), Hex("c5cae9"), Hex("9fa8da"), Hex("7986cb"), Hex("5c6bc0"), Hex("3949ab"), Hex("303f9f"), Hex("283593"), Hex("1a237e") };
+            }
+            else if (col == Colours.Blue)
+            {
+                shades = new List<Color>() { Hex("e3f2fd"), Hex("bbdefb"), Hex("90caf9"), Hex("64b5f6"), Hex("42a5f5"), Hex("1e88e5"), Hex("1976d2"), Hex("1565c0"), Hex("0d47a1") };
+            }
+            else if (col == Colours.Cyan)
+            {
+                shades = new List<Color>() { Hex("e0f7fa"), Hex("b2ebf2"), Hex("80deea"), Hex("4dd0e1"), Hex("26c6da"), Hex("00acc1"), Hex("0097a7"), Hex("00838f"), Hex("006064") };
+            }
+            else if (col == Colours.Teal)
+            {
+                shades = new List<Color>() { Hex("e0f2f1"), Hex("b2dfdb"), Hex("80cbc4"), Hex("4db6ac"), Hex("26a69a"), Hex("00897b"), Hex("00796b"), Hex("00695c"), Hex("#004d40") };
+            }
+            else if (col == Colours.Green)
+            {
+                shades = new List<Color>() { Hex("e8f5e9"), Hex("c8e6c9"), Hex("a5d6a7"), Hex("81c784"), Hex("66bb6a"), Hex("43a047"), Hex("388e3c"), Hex("2e7d32"), Hex("1b5e20") };
+            }
+            else if (col == Colours.Yellow)
+            {
+                shades = new List<Color>() { Hex("fffde7"), Hex("fff9c4"), Hex("fff59d"), Hex("fff176"), Hex("ffee58"), Hex("fdd835"), Hex("fbc02d"), Hex("f9a825"), Hex("f57f17") };
+            }
+            else if (col == Colours.Orange)
+            {
+                shades = new List<Color>() { Hex("fff3e0"), Hex("ffe0b2"), Hex("ffcc80"), Hex("ffb74d"), Hex("ffa726"), Hex("fb8c00"), Hex("f57c00"), Hex("ef6c00"), Hex("e65100") };
+            }
+            else if (col == Colours.Grey)
+            {
+                shades = new List<Color>() { Hex("fafafa"), Hex("f5f5f5"), Hex("eeeeee"), Hex("e0e0e0"), Hex("9e9e9e"), Hex("757575"), Hex("616161"), Hex("424242"), Hex("212121") };
+            }
+            else if (col == Colours.BlueGrey)
+            {
+                shades = new List<Color>() { Hex("eceff1"), Hex("cfd8dc"), Hex("b0bec5"), Hex("90a4ae"), Hex("78909c"), Hex("546e7a"), Hex("455a64"), Hex("37474f"), Hex("263238") };
+            }
+            else if (col == Colours.Brown)
+            {
+                shades = new List<Color>() { Hex("efebe9"), Hex("d7ccc8"), Hex("bcaaa4"), Hex("a1887f"), Hex("8d6e63"), Hex("6d4c41"), Hex("5d4037"), Hex("4e342e"), Hex("3e2723") };
+            }
+            else
+            {
+                return shades = new List<Color>() { col.Lighten(.8f), col.Lighten(.6f), col.Lighten(.4f), col.Lighten(.2f), col, col.Darken(.2f), col.Darken(.4f), col.Darken(.6f), col.Darken(.8f) };
 
             }
+
+
 
 
             if (includeBase)
@@ -150,17 +154,15 @@ namespace Dutil
         /// <returns></returns>/
         public static Color Shade(this Color col, int index, bool includeBase = false)
         {
-            if (Colours.All.Contains(col))
+
+            List<Color> shades = col.Shades().Copy();
+            if (includeBase && Colours.All.Contains(col))
             {
-                List<Color> shades = col.Shades().Copy();
-                if (includeBase)
-                {
-                    shades.Insert(5, col);
-                }
-                if (index < shades.Count)
-                {
-                    return shades[index];
-                }
+                shades.Insert(5, col);
+            }
+            if (index < shades.Count)
+            {
+                return shades[index];
             }
 
             return col;
