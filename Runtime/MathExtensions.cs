fd8eff6e60ade8b9ae7fef5d7028a802a086fb4c
DannyBoyThomas <danielt1melb@gmail.com>
2023-07-14 16:59:00 +1000

    More Math Extensions

--- Runtime/MathExtensions.cs
+++ Runtime/MathExtensions.cs
@@ -7,12 +7,29 @@ namespace Dutil
     public static class MathExtensions
     {
         public static float Clamp(this float f) => Mathf.Clamp01(f);
+        public static float Clamp(this float f, float min, float max) => Mathf.Clamp(f, min, max);
+        public static int Clamp(this int f, int min, int max) => Mathf.Clamp(f, min, max);
         public static float Square(this float f) => f * f;
         public static float SquareRoot(this float f) => Mathf.Sqrt(f);
         public static float To(this float f, float p) => Mathf.Pow(f, p);
+        public static float To(this int f, float p) => Mathf.Pow(f, p);
         public static bool Between(this float f, float min, float max) => f >= min && f <= max;
-        public static float Map(this float f, float oldMin, float oldMax, float newMin, float newMax) => (f - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin;
-        public static float Map(this int f, float oldMin, float oldMax, float newMin, float newMax) => (f - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin;
+        // public static float Map(this float f, float oldMin, float oldMax, float newMin, float newMax) => (f - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin;
+        // public static float Map(this int f, float oldMin, float oldMax, float newMin, float newMax) => (f - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin;
+        public static float Map(this float f, float oldMin, float oldMax, float newMin, float newMax, bool clamp = false)
+        {
+            float val = (f - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin;
+            if (clamp)
+            {
+                val = val.Clamp(newMin, newMax);
+            }
+            return val;
+        }
+        public static float Map(this int f, float oldMin, float oldMax, float newMin, float newMax, bool clamp = false)
+        {
+            return ((float)f).Map(oldMin, oldMax, newMin, newMax, clamp);
+        }
+        public static int Round(this float f) => Mathf.RoundToInt(f);
         public static Vector3 X(this float f) => new Vector3(f, 0, 0);
         public static Vector3 Y(this float f) => new Vector3(0, f, 0);
         public static Vector3 Z(this float f) => new Vector3(0, 0, f);
